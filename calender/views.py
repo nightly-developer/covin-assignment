2c45ce43525a86a128f5685b3e87523f5ec1357b
rick <xyz@abc.com>
2023-05-27 16:31:14 +0530

    generate client ID and client secret

--- calender/views.py
+++ calender/views.py
@@ -1,8 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-@api_view(['GET'])
-def getData(request):
-    person = {'userId':'vk','password':'123'}
-    return Response(person)
+# endpoints and credentials
+CLIENT_ID = '89335088404-0vgojdriovtf1psnco7v150468u4fs09.apps.googleusercontent.com'
+CLIENT_SECRET = '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'
+REDIRECT_URI = 'http://localhost:8000/rest/v1/calendar/redirect/'
+SCOPE = 'https://www.googleapis.com/auth/calendar.readonly'
 
