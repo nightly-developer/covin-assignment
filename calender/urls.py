4cadaa99e3ae31ad9847afa65c8d613155c9c308
rick <xyz@abc.com>
2023-05-28 19:15:48 +0530

    second approach

--- calender/urls.py
+++ calender/urls.py
@@ -3,6 +3,7 @@ from . import views
 from .views import GoogleCalendarInitView,GoogleCalendarRedirectView
 
 urlpatterns = [
-    path('', GoogleCalendarInitView.as_view(), name='google_calendar_init'),
-    path('', GoogleCalendarRedirectView.as_view(), name='google_calendar_redirect'),
+    # path('', views.home, name='home'),
+    path('rest/v1/calendar/init/', GoogleCalendarInitView.as_view(), name='google_calendar_init'),
+    path('rest/v1/calendar/redirect/', GoogleCalendarRedirectView.as_view(), name='google_calendar_redirect'),
 ]
--- calender/views.py
+++ calender/views.py
@@ -1,58 +1,26 @@
-from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.views import View
+from google_auth_oauthlib.flow import Flow
+from googleapiclient.discovery import build
 from google.auth.transport import requests
-from django.contrib import messages
 
-
-# from rest_framework.response import Response
-# from rest_framework.decorators import api_view
-
-# endpoints and credentials
+# Constants for Google OAuth2
 CLIENT_ID = '89335088404-0vgojdriovtf1psnco7v150468u4fs09.apps.googleusercontent.com'
 CLIENT_SECRET = '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'
 REDIRECT_URI = 'http://localhost:8000/rest/v1/calendar/redirect/'
-SCOPE = 'https://www.googleapis.com/auth/calendar.readonly'
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-# prompt user for his/her credentials
 class GoogleCalendarInitView(View):
     def get(self, request):
-        # Redirect user to Google for authentication
-        auth_url = f'https://accounts.google.com/o/oauth2/auth?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope={SCOPE}&response_type=code'
-        return HttpResponseRedirect(auth_url)
-    
-class GoogleCalendarRedirectView(View):
-    def get(self, request):
-        # Retrieve the authorization code from the redirect URL
-        code = request.GET.get('code')
-
-        # Exchange authorization code for access token
-        token_url = 'https://accounts.google.com/o/oauth2/token'
-        token_data = {
-            'code': code,
-            'client_id': CLIENT_ID,
-            'client_secret': CLIENT_SECRET,
-            'redirect_uri': REDIRECT_URI,
-            'grant_type': 'authorization_code'
-        }
-        response = requests.post(token_url, data=token_data)
-
-        if response.status_code == 200:
-            # Get access token from the response
-            access_token = response.json().get('access_token')
-
-            # Retrieveing calendar events
-            events_url = 'https://www.googleapis.com/calendar/v3/calendars/primary/events'
-            headers = {'Authorization': f'Bearer {access_token}'}
-            events_response = requests.get(events_url, headers=headers)
-
-            if events_response.status_code == 200:
-                # Process the calendar events
-                events = events_response.json().get('items')
-
-                # Return the list of events as JSON response
-                return JsonResponse({'events': events})
-            else:
-                return HttpResponse('Failed to fetch calendar events', status=events_response.status_code)
-
-        else:
-            return HttpResponse('Failed to exchange authorization code for access token', status=response.status_code)
+        flow = Flow.from_client_secrets_file(
+            'calender/client_secret.json',
+            scopes=SCOPES,
+            redirect_uri=REDIRECT_URI
+        )
+        authorization_url, state = flow.authorization_url(
+            access_type='offline',
+            include_granted_scopes='true'
+        )
+        request.session['oauth2_state'] = state
+        return HttpResponseRedirect(authorization_url)
